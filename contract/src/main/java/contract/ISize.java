0cb5549e18c78d6501708be8e777a708fa2024ff
Théo <44427781+Theo76500@users.noreply.github.com>
2019-05-26 00:09:27 +0200

    COMMIT 12
    - Add Size and ISize for the dimension of the level

---
+++ contract/src/main/java/contract/ISize.java
@@ -0,0 +1,19 @@
+package contract;
+
+/**
+ * Interface size of the screen
+ * 
+ * @author Langlois Theo
+ *
+ */
+
+public interface ISize {
+	int getWidth();
+	
+	void setWidth (int width);
+	
+	int getHeight();
+	
+	void setHeight(int height);
+
+}
